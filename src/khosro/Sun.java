252b15b77c7261ad6eeee3edc13c1a9c6ece7ef5
Haj-Rezvan <58388933+hajrezvan@users.noreply.github.com>
2021-01-11 09:42:15 +0330

    Add fields.

--- src/khosro/Sun.java
+++ src/khosro/Sun.java
@@ -1,9 +1,14 @@
 package khosro;
 
+import khosro.map.MapHome;
+
 import java.awt.*;
 
 public class Sun {
     private Image image;
+    private MapHome locationX;
+    private MapHome locationY;
+    private final int disappearTimer = 5;
 
     public Sun(){
 
