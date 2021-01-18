1b3c459c9c1117b23dca7064dc2ebea5959eeb93
khosravi-am <58389845+khosravi-am@users.noreply.github.com>
2021-01-18 16:37:25 +0330

    Update MapRow.java

--- src/khosro/map/MapRow.java
+++ src/khosro/map/MapRow.java
@@ -8,11 +8,11 @@ import java.util.ArrayList;
 
 public class MapRow {
     private ArrayList<MapHome> mapHomes;
-    private Image image;
-    private int count,x1,x2,y1,y2;
+    private Image image,image2;
+    private int count, x, width, y, height;
 
-    public MapRow(int count){
-        this.count=count;
+    public MapRow(int count) {
+        this.count = count;
         setXY(count);
         setMapHomes();
         setImage();
@@ -20,14 +20,48 @@ public class MapRow {
 
     private void setImage() {
         try {
-            image=ImageIO.read(new File("./images/car.png"));
+            image = ImageIO.read(new File("./images/car.png"));
+            if (count==0)
+                image2=ImageIO.read(new File("./images/row-1.png"));
+            if (count==1)
+                image2=ImageIO.read(new File("./images/row-2.png"));
+            if (count==2)
+                image2=ImageIO.read(new File("./images/row-3.png"));
+            if (count==3)
+                image2=ImageIO.read(new File("./images/row-4.png"));
+            if (count==4)
+                image2=ImageIO.read(new File("./images/row-5.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
+    public Image getImage2() {
+        return image2;
+    }
+
     private void setXY(int count) {
+        if (count == 0) {
+            y = 95;
+            height = 147;
+        }
+        if (count == 1) {
+            y = 240;
+            height = 130;
+        }
+        if (count == 2) {
+            y = 360;
+            height = 130;
+        }
+        if (count == 3) {
+            y = 480;
+            height = 125;
+        }
+        if (count == 4) {
+            y = 595;
+            height = 145;
+        }
 
     }
 
@@ -43,20 +77,20 @@ public class MapRow {
         return count;
     }
 
-    public int getX1() {
-        return x1;
+    public int getX() {
+        return x;
     }
 
-    public int getY2() {
-        return y2;
+    public int getHeight() {
+        return height;
     }
 
-    public int getX2() {
-        return x2;
+    public int getWidth() {
+        return width;
     }
 
-    public int getY1() {
-        return y1;
+    public int getY() {
+        return y;
     }
 
     public ArrayList<MapHome> getMapHomes() {
@@ -64,11 +98,11 @@ public class MapRow {
     }
 
     private void setMapHomes() {
-        mapHomes=new ArrayList<>();
-        for (int i=0;i<9;i++) {
-            mapHomes.add(new MapHome(i + 1));
+        mapHomes = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            mapHomes.add(new MapHome(i + 1, y, height));
             try {
-                mapHomes.get(i).setImage(ImageIO.read(new File("./images/"+String.valueOf(count)+","+String.valueOf(i)+".png")));
+                mapHomes.get(i).setImage(ImageIO.read(new File("./images/" + String.valueOf(count) + "," + String.valueOf(i) + ".png")));
             } catch (IOException e) {
                 e.printStackTrace();
             }
