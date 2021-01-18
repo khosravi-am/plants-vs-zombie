19f012c12bcb0c9ffe74a8c4dc7dc8d219c9098d
khosravi-am <58389845+khosravi-am@users.noreply.github.com>
2021-01-18 16:34:50 +0330

    Update GameHandler.java

--- src/khosro/handler/GameHandler.java
+++ src/khosro/handler/GameHandler.java
@@ -13,7 +13,7 @@ public class GameHandler {
     private Map map;
     private Graphics2D g2d;
     private GamePage gamePage;
-
+    Long start;
     /*private BufferedImage image;*/
 
     private BufferStrategy bufferStrategy;
@@ -21,38 +21,87 @@ public class GameHandler {
     public GameHandler(Map map, GamePage gamePage) {
         this.map = map;
         this.gamePage = gamePage;
+        start = System.currentTimeMillis();
         setImage();
-       // initBufferStrategy();
+        initBufferStrategy();
 
     }
 
 
     /**
      * This must be called once after the JFrame is shown:
-     *    frame.setVisible(true);
+     * frame.setVisible(true);
      * and before any rendering is started.
      */
     public void initBufferStrategy() {
         // Triple-buffering
         gamePage.createBufferStrategy(3);
         bufferStrategy = gamePage.getBufferStrategy();
+        g2d = (Graphics2D) bufferStrategy.getDrawGraphics();
+
     }
 
 
     /**
      * Game rendering with triple-buffering using BufferStrategy.
+     *
+     * @
+     * @param i
+     * @param cf
+     * @param cc
+     * @param cp
+     * @param f
+     * @param p
      */
-    public void render(){
-        gamePage.paint2();
+    public void render(int cf, int cc, int cp, int f, int p,int l) {
+        // Render single frame
+        do {
+            // The following loop ensures that the contents of the drawing buffer
+            // are consistent in case the underlying surface was recreated
+            do {
+                // Get a new graphics context every time through the loop
+                // to make sure the strategy is validated
+                g2d = (Graphics2D) bufferStrategy.getDrawGraphics();
+                try {
+                    //doRendering(graphics, state);
+                    gamePage.paintBack(g2d);
+                    gamePage.setRows(g2d, map.getMapRows());
+                    gamePage.paintMenu(g2d);
+                    gamePage.delayCardFlower(g2d,cf);
+                    gamePage.delayCardCherry(g2d,cc);
+                    gamePage.delayCardPea(g2d,cp);
+                    gamePage.delayCardFreeze(g2d,f);
+                    gamePage.delayCardPotato(g2d,p);
+                    gamePage.paintLevel(g2d,l);
 
+                } finally {
+                    // Dispose the graphics
+                    g2d.dispose();
+                }
+                // Repeat the rendering if the drawing buffer contents were restored
+            } while (bufferStrategy.contentsRestored());
+
+            // Display the buffer
+            bufferStrategy.show();
+            // Tell the system to do the drawing NOW;
+            // otherwise it can take a few extra ms and will feel jerky!
+            Toolkit.getDefaultToolkit().sync();
+
+            // Repeat the rendering if the drawing buffer was lost
+        } while (bufferStrategy.contentsLost());
     }
-    public void render2(){
-        gamePage.paint3();
-    }
+
     private void setImage() {
         gamePage.setBackground(map.getBackground().getImage());
-        gamePage.setMapRow(map.getMapRows());
+        gamePage.setMenu(map.getMenu().getImage());
+        gamePage.setMenuBar(map.getMenuBar().getImage());
+        gamePage.setShovel(map.getShovel().getImage());
         gamePage.setCardFlower(map.getCardFlower().getImage());
+        gamePage.setLevel(map.getLevel().getImage());
+        gamePage.setCardCherry(map.getCardCherry().getImage());
+        gamePage.setCardFreeze(map.getCardFreeze().getImage());
+        gamePage.setCardPea(map.getCardPea().getImage());
+        gamePage.setCardPotato(map.getCardPotato().getImage());
         //gamePage.setVisible(true);
     }
 
