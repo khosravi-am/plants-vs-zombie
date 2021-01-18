ae6330de35648337b5a02443aaf65132b79b7c9f
khosravi-am <58389845+khosravi-am@users.noreply.github.com>
2021-01-18 16:37:49 +0330

    Update MapHome.java

--- src/khosro/map/MapHome.java
+++ src/khosro/map/MapHome.java
@@ -7,15 +7,65 @@ import java.io.IOException;
 
 public class MapHome {
     private Image image;
-    private int count, x1, width, height, num;
+    private int count, x,y, width, height;
 
-    public MapHome(int count) {
+    public MapHome(int count,int y,int height) {
         this.count = count;
+        this.y=y;
+        this.height=height;
         setXY(count);
         setImage(count);
     }
 
     private void setXY(int count) {
+        if (count == 1) {
+            x = 35;
+            width=125;
+        }
+
+        if (count == 2) {
+            x = 155;
+            width=110;
+            if (y>500) {
+                x = 150;
+                width=117;
+            }
+        }
+        if (count == 3) {
+            x = 260;
+            width=125;
+            if (y>500){
+                x=255;
+                width=135;
+            }
+        }
+        if (count == 4) {
+            x = 380;
+            width=107;
+        }
+        if (count == 5) {
+            x = 475;
+            width=133;
+        }
+        if (count == 6) {
+            x = 595;
+            width=123;
+        }
+        if (count == 7) {
+            x = 705;
+            width=120;
+        }
+        if (count == 8) {
+            x = 810;
+            width=123;
+        }
+
+        if (count == 9) {
+            x = 915;
+            width=125;
+            if (y>100)
+                width=137;
+        }
 
     }
 
@@ -32,16 +82,20 @@ public class MapHome {
         return count;
     }
 
-    public int getX1() {
-        return x1;
+    public int getX() {
+        return x;
     }
 
-    public int getX2() {
+    public int getWidth() {
         return width;
     }
 
-    public int getY2() {
+    public int getHeight() {
         return height;
+    }
+
+    public int getY() {
+        return y;
     }
 
     public void setImage(Image image) {
