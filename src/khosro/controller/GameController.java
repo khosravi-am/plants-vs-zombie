a1a827a4bb1dab7be3fead6fad6fd50ee0ce4ae9
khosravi-am <58389845+khosravi-am@users.noreply.github.com>
2021-01-23 19:49:28 +0330

    Update GameController.java

--- src/khosro/controller/GameController.java
+++ src/khosro/controller/GameController.java
@@ -1,8 +1,9 @@
 package khosro.controller;
 
-import khosro.model.component.plant.Plants;
-import khosro.model.component.plant.SunFlower;
+import khosro.Main;
+import khosro.model.component.plant.*;
 import khosro.model.map.Map;
+import khosro.model.map.MapHome;
 import khosro.views.GamePage;
 
 
@@ -13,32 +14,39 @@ import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferStrategy;
 import java.util.ArrayList;
 
-public class GameController implements MouseListener,MouseMotionListener {
+public class GameController implements MouseListener, MouseMotionListener {
     public boolean gameOver;
-    private ArrayList<Plants> pea,sunflower,freeze,potato,cherry;
-    private Boolean clickPea=false,clickSunflower=false,clickFreeze=false,clickPotato=false,clickCherry=false;
+    private ArrayList<Plants> pea, sunflower, freeze, potato, cherry;
+    private Boolean clickPea = false, clickSunflower = false, clickFreeze = false, clickPotato = false, clickCherry = false;
     private Map map;
     private Graphics2D g2d;
     private GamePage gamePage;
     Long start;
+    private int mouseX, mouseY;
     /*private BufferedImage image;*/
 
     private BufferStrategy bufferStrategy;
 
-    public GameController(Map map, GamePage gamePage){
+    public GameController(Map map, GamePage gamePage) {
         this.map = map;
         this.gamePage = gamePage;
         start = System.currentTimeMillis();
         gamePage.addMouseListener(this);
         gamePage.addMouseMotionListener(this);
-        pea=new ArrayList<>();
-        sunflower=new ArrayList<>();
-        freeze=new ArrayList<>();
-        potato=new ArrayList<>();
-        cherry=new ArrayList<>();
+        pea = new ArrayList<>();
+        sunflower = new ArrayList<>();
+        freeze = new ArrayList<>();
+        potato = new ArrayList<>();
+        cherry = new ArrayList<>();
         setImage();
         initBufferStrategy();
+        //(addMouseListener();
+    }
 
+    private void addMouseListener() {
+      /*  for (int i=0;i<5;i++)
+            for (int j=0;j<9;j++)
+      */          //map.getMapRows().get(i).getMapHomes().get(j).addMouseListener(this);
     }
 
 
@@ -65,7 +73,6 @@ public class GameController implements MouseListener,MouseMotionListener {
      * @param f
      * @param p
      * @param l
-     *
      */
     public void render(int cf, int cc, int cp, int f, int p, int l) {
         // Render single frame
@@ -77,7 +84,6 @@ public class GameController implements MouseListener,MouseMotionListener {
                 // to make sure the strategy is validated
                 g2d = (Graphics2D) bufferStrategy.getDrawGraphics();
                 try {
-                    //doRendering(graphics, state);
                     gamePage.paintBack(g2d);
                     gamePage.setRows(g2d, map.getMapRows());
                     gamePage.paintMenu(g2d);
@@ -87,8 +93,19 @@ public class GameController implements MouseListener,MouseMotionListener {
                     gamePage.delayCardFreeze(g2d, f);
                     gamePage.delayCardPotato(g2d, p);
                     gamePage.paintLevel(g2d, l);
-                    //if (clickSunflower)
-                       // paintFlower(new SunFlower())
+                    if (clickSunflower)
+                        paintFlower(new SunFlower());
+
+                    if (sunflower.size() > 0 && sunflower.get(sunflower.size() - 1).isUse())
+                        gamePage.paintMouseImg(g2d, sunflower.get(sunflower.size() - 1).getImage().getImage(), mouseX - 50, mouseY - 80);
+
+                    for (int i = 0; i < 5; i++)
+                        for (int j = 0; j < 9; j++)
+                            if (map.getMapRows().get(i).getMapHomes().get(j).getSunflower())
+                                checkAndInsert(map.getMapRows().get(i).getMapHomes().get(j));
+
+
+                } catch (NullPointerException e) {
 
                 } finally {
                     // Dispose the graphics
@@ -107,6 +124,46 @@ public class GameController implements MouseListener,MouseMotionListener {
         } while (bufferStrategy.contentsLost());
     }
 
+    private void paintFlower(SunFlower sunFlower) {
+        clickSunflower = false;
+        sunFlower.setUse(true);
+        sunflower.add(sunFlower);
+        gamePage.paintMouseImg(g2d, sunFlower.getImage().getImage(), mouseX - 50, mouseY - 80);
+
+    }
+
+    private void paintPea(Peashooter peashooter) {
+        clickPea = false;
+        peashooter.setUse(true);
+        pea.add(peashooter);
+        gamePage.paintMouseImg(g2d, peashooter.getImage().getImage(), mouseX - 50, mouseY - 80);
+
+    }
+
+    private void paintFreeze(SnowPea snowPea) {
+        clickFreeze = false;
+        snowPea.setUse(true);
+        freeze.add(snowPea);
+        gamePage.paintMouseImg(g2d, snowPea.getImage().getImage(), mouseX - 50, mouseY - 80);
+
+    }
+
+    private void paintCherry(Cherry cherry) {
+        clickCherry= false;
+        cherry.setUse(true);
+        this.cherry.add(cherry);
+        gamePage.paintMouseImg(g2d, cherry.getImage().getImage(), mouseX - 50, mouseY - 80);
+
+    }
+
+    private void paintPotato(Potato potato) {
+        clickSunflower = false;
+        potato.setUse(true);
+        this.potato.add(potato);
+        gamePage.paintMouseImg(g2d, potato.getImage().getImage(), mouseX - 50, mouseY - 80);
+
+    }
+
     private void setImage() {
         gamePage.setBackground(map.getBackground().getImage());
         gamePage.setMenu(map.getMenu().getImage());
@@ -121,6 +178,11 @@ public class GameController implements MouseListener,MouseMotionListener {
         //gamePage.setVisible(true);
     }
 
+    private void checkAndInsert(MapHome mapHome) {
+        gamePage.paintHomeImage(g2d, sunflower.get(sunflower.size() - 1).getGif().getImage(), mapHome.getX(), mapHome.getY());
+
+    }
+
 
     public void update() {
     }
@@ -133,9 +195,8 @@ public class GameController implements MouseListener,MouseMotionListener {
      */
     @Override
     public void mouseClicked(MouseEvent e) {
-        System.out.println("x:"+e.getX()+" y:"+e.getY());
-        //if (e.getX()>132 && e.getY()>41 && e.getX()<196 && e.getY()<122)
-           // clickSunflower=true;
+        System.out.println("x:" + e.getX() + " y:" + e.getY());
+
     }
 
     /**
@@ -146,11 +207,43 @@ public class GameController implements MouseListener,MouseMotionListener {
     @Override
     public void mousePressed(MouseEvent e) {
         //System.out.println("x:"+e.getX()+" y:"+e.getY());
-        if (e.getX()>132 && e.getY()>41 && e.getX()<196 && e.getY()<122)
-            clickSunflower=true;
+        if (e.getX() > 132 && e.getY() > 40 && e.getX() < 196 && e.getY() < 125 && Main.card1)
+            clickSunflower = true;
+        if (e.getX() > 210 && e.getY() > 40 && e.getX() < 275 && e.getY() < 125 && Main.card2)
+            clickPea = true;
+        if (e.getX() > 290 && e.getY() > 40 && e.getX() < 360 && e.getY() < 125 && Main.card3)
+            clickFreeze = true;
+        if (e.getX() > 370 && e.getY() > 40 && e.getX() < 440 && e.getY() < 125 && Main.card4)
+            clickCherry = true;
+        if (e.getX() > 450 && e.getY() > 40 && e.getX() < 515 && e.getY() < 125 && Main.card5)
+            clickPotato = true;
+        if (e.getX() > 45 && e.getY() > 137 && e.getX() < 1042 && e.getY() < 734)
+            checkHome(e);
 
-        System.out.println(clickSunflower);
+    }
 
+    private void checkHome(MouseEvent e) {
+        for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 9; j++)
+                if (e.getX() > map.getMapRows().get(i).getMapHomes().get(j).getX() && e.getY() > map.getMapRows().get(i).getMapHomes().get(j).getY() && e.getX() < map.getMapRows().get(i).getMapHomes().get(j).getX2() && e.getY() < map.getMapRows().get(i).getMapHomes().get(j).getY2()) {
+                    if (!map.getMapRows().get(i).getMapHomes().get(j).isPor()) {
+                        map.getMapRows().get(i).getMapHomes().get(j).setSunflower(sunflower.get(sunflower.size() - 1).isUse());
+                        sunflower.get(sunflower.size() - 1).setUse(false);
+                    }
+                    break;
+                }
+    }
+
+    public Boolean getClickSunflower() {
+        return clickSunflower;
+    }
+
+    public ArrayList<Plants> getSunflower() {
+        return sunflower;
+    }
+
+    public ArrayList<Plants> getPea() {
+        return pea;
     }
 
     /**
@@ -160,7 +253,7 @@ public class GameController implements MouseListener,MouseMotionListener {
      */
     @Override
     public void mouseReleased(MouseEvent e) {
-
+        //checkHome(e);
     }
 
     /**
@@ -198,6 +291,8 @@ public class GameController implements MouseListener,MouseMotionListener {
      */
     @Override
     public void mouseDragged(MouseEvent e) {
+        mouseX = e.getX();
+        mouseY = e.getY();
 
     }
 
@@ -209,6 +304,7 @@ public class GameController implements MouseListener,MouseMotionListener {
      */
     @Override
     public void mouseMoved(MouseEvent e) {
-        System.out.println(e.getX());
+        mouseX = e.getX();
+        mouseY = e.getY();
     }
 }
