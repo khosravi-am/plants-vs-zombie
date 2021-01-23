6539072a89416bbb8bd5ec17533485f4880384e3
khosravi-am <58389845+khosravi-am@users.noreply.github.com>
2021-01-23 19:43:34 +0330

    Update MainPage.java

--- src/khosro/views/MainPage.java
+++ src/khosro/views/MainPage.java
@@ -1,10 +1,109 @@
+/*** In The Name of Allah ***/
 package khosro.views;
 
-import javax.swing.*;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferStrategy;
+import java.util.ArrayList;
+import javax.swing.*;
 
-public class MainPage extends JFrame implements MouseListener {
+/**
+ * The window on which the rendering is performed.
+ * This example uses the modern BufferStrategy approach for double-buffering,
+ * actually it performs triple-buffering!
+ * For more information on BufferStrategy check out:
+ *    http://docs.oracle.com/javase/tutorial/extra/fullscreen/bufferstrategy.html
+ *    http://docs.oracle.com/javase/8/docs/api/java/awt/image/BufferStrategy.html
+ *
+ * @author Seyed Mohammad Ghaffarian
+ */
+public class MainPage extends JFrame implements MouseMotionListener, MouseListener {
+
+    //uncomment all /*...*/ in the class for using Tank icon instead of a simple circle
+    /*private BufferedImage image;*/
+    private ImageIcon icon;
+    private Boolean login,setting,rank,runGame,loadGame;
+    private Graphics2D graphics;
+    private BufferStrategy bufferStrategy;
+
+    public MainPage() {
+        super("Plants vs. Zombie");
+        icon=new ImageIcon("./src/khosro/model/res/icon.png");
+        super.setIconImage(icon.getImage());
+        super.setSize(1080, 770);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super.setLocationRelativeTo(null);
+        super.setLayout(null);
+        super.setResizable(false);
+        super.setVisible(true);
+
+        initBufferStrategy();
+
+	/*	try{
+			image = ImageIO.read(new File("Icon.png"));
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}*/
+    }
+    /**
+     * This must be called once after the JFrame is shown:
+     *    frame.setVisible(true);
+     * and before any rendering is started.
+     */
+    public void initBufferStrategy() {
+        // Triple-buffering
+        this.createBufferStrategy(3);
+        bufferStrategy = this.getBufferStrategy();
+    }
+
+
+    /**
+     * Game rendering with triple-buffering using BufferStrategy.
+     */
+    public void render() {
+        // Render single frame
+        do {
+            // The following loop ensures that the contents of the drawing buffer
+            // are consistent in case the underlying surface was recreated
+            do {
+                // Get a new graphics context every time through the loop
+                // to make sure the strategy is validated
+                graphics = (Graphics2D) bufferStrategy.getDrawGraphics();
+                try {
+                    doRendering(graphics);
+                } finally {
+                    // Dispose the graphics
+                    graphics.dispose();
+                }
+                // Repeat the rendering if the drawing buffer contents were restored
+            } while (bufferStrategy.contentsRestored());
+
+            // Display the buffer
+            bufferStrategy.show();
+            // Tell the system to do the drawing NOW;
+            // otherwise it can take a few extra ms and will feel jerky!
+            Toolkit.getDefaultToolkit().sync();
+
+            // Repeat the rendering if the drawing buffer was lost
+        } while (bufferStrategy.contentsLost());
+    }
+
+    /**
+     * Rendering all game elements based on the game state.
+     */
+    private void doRendering(Graphics2D g2d) {
+
+    }
+
+
+    public Boolean getRunGame() {
+        return runGame;
+    }
+
     /**
      * Invoked when the mouse button has been clicked (pressed
      * and released) on a component.
@@ -53,6 +152,35 @@ public class MainPage extends JFrame implements MouseListener {
      */
     @Override
     public void mouseExited(MouseEvent e) {
+
+    }
+
+    /**
+     * Invoked when a mouse button is pressed on a component and then
+     * dragged.  {@code MOUSE_DRAGGED} events will continue to be
+     * delivered to the component where the drag originated until the
+     * mouse button is released (regardless of whether the mouse position
+     * is within the bounds of the component).
+     * <p>
+     * Due to platform-dependent Drag&amp;Drop implementations,
+     * {@code MOUSE_DRAGGED} events may not be delivered during a native
+     * Drag&amp;Drop operation.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    /**
+     * Invoked when the mouse cursor has been moved onto a component
+     * but no buttons have been pushed.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseMoved(MouseEvent e) {
 
     }
 }
