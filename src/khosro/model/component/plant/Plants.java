f549763e0d0e8a2cac816df2103c753c6800afe2
khosravi-am <58389845+khosravi-am@users.noreply.github.com>
2021-01-23 19:47:14 +0330

    Update Plants.java

--- src/khosro/model/component/plant/Plants.java
+++ src/khosro/model/component/plant/Plants.java
@@ -31,7 +31,8 @@ public class Plants {
     /**
      * Image of plants.
      */
-    protected Image image;
+    protected ImageIcon image;
+    protected ImageIcon gif;
 
     /**
      * Cost of each plants per sun
@@ -67,6 +68,8 @@ public class Plants {
      * X location in map
      */
     protected int x;
+    protected int width;
+    protected int height;
 
     /**
      * Y location in map
@@ -75,20 +78,22 @@ public class Plants {
 
     protected boolean live;
 
+    protected boolean use=false;
+
     /**
-     * @param bulletArr     Array of bullet that plant can shoot it. If plant can't shoot a bullet, it equal to null.
-     * @param img           Image address.
-     * @param image         Image of plants.
-     * @param life          Each plants how many life time when zombies eat it.
-     * @param locX          X location in visual map
-     * @param locY          Y location in visual map
-     * @param bornTime      Time has left a plants card refresh.
-     * @param x             X location in map
-     * @param y             Y location in map
+     * @param bulletArr Array of bullet that plant can shoot it. If plant can't shoot a bullet, it equal to null.
+     * @param img       Image address.
+     * @param image     Image of plants.
+     * @param life      Each plants how many life time when zombies eat it.
+     * @param locX      X location in visual map
+     * @param locY      Y location in visual map
+     * @param bornTime  Time has left a plants card refresh.
+     * @param x         X location in map
+     * @param y         Y location in map
      */
     public Plants(ArrayList<Bullet> bulletArr,
                   BufferedImage img,
-                  Image image,
+                  ImageIcon image,
                   int life,
                   MapHome locX,
                   MapHome locY,
@@ -112,7 +117,11 @@ public class Plants {
             e.printStackTrace();
         }
 
-        image = new ImageIcon("input.gif").getImage();
+        image = new ImageIcon("input.gif");
+    }
+
+    public Plants() {
+
     }
 
     public MapHome getLocX() {
@@ -135,12 +144,116 @@ public class Plants {
         return live;
     }
 
+    public ArrayList<Bullet> getBulletArr() {
+        return bulletArr;
+    }
+
+    public void setBulletArr(ArrayList<Bullet> bulletArr) {
+        this.bulletArr = bulletArr;
+    }
+
+    public BufferedImage getImg() {
+        return img;
+    }
+
+    public void setImg(BufferedImage img) {
+        this.img = img;
+    }
+
+    public ImageIcon getImage() {
+        return image;
+    }
+
+    public void setImage(ImageIcon image) {
+        this.image = image;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getPreparingTime() {
+        return preparingTime;
+    }
+
+    public void setPreparingTime(int preparingTime) {
+        this.preparingTime = preparingTime;
+    }
+
+    public void setLocX(MapHome locX) {
+        this.locX = locX;
+    }
+
+    public void setLocY(MapHome locY) {
+        this.locY = locY;
+    }
+
+    public long getBornTime() {
+        return bornTime;
+    }
+
+    public void setBornTime(long bornTime) {
+        this.bornTime = bornTime;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int high) {
+        this.height = high;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public boolean isUse() {
+        return use;
+    }
+
+    public void setUse(boolean use) {
+        this.use = use;
+    }
+
     public void setLive(boolean live) {
         this.live = live;
     }
 
+    public ImageIcon getGif() {
+        return gif;
+    }
+
+    public void setGif(ImageIcon gif) {
+        this.gif = gif;
+    }
+
+    //TODO remove plant form screen.
     public void dead() {
-        //TODO remove plant form screen.
         live = false;
     }
 }
