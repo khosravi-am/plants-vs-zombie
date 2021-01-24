37f1187e558763497ce3b039e56ee81ac43b0dd1
khosravi-am <58389845+khosravi-am@users.noreply.github.com>
2021-01-24 09:04:00 +0330

    Update MapHome.java

--- src/khosro/model/map/MapHome.java
+++ src/khosro/model/map/MapHome.java
@@ -1,20 +1,29 @@
 package khosro.model.map;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
 public class MapHome {
     private Image image;
-    private int count, x,y, width, height;
-
+    private int count, x,y,x2,y2, width, height;
+    private Boolean click=false,sunflower=false,pea=false,freeze=false,potato=false,cherry=false;
     public MapHome(int count,int y,int height) {
         this.count = count;
         this.y=y;
         this.height=height;
         setXY(count);
         setImage(count);
+      /*  setFocusable(false);
+        setLayout(null);
+        setBounds(x,y,width-10,height-30);
+        if (y>115)
+            setBounds(x,y-30,width-10,height);
+      */  //setFocusPainted(false);
+        x2=x+width;
+        y2=y+height;
     }
 
     private void setXY(int count) {
@@ -74,6 +83,20 @@ public class MapHome {
 
     }
 
+    public Boolean isPor(){
+        if (sunflower||pea||potato||cherry||freeze)
+            return true;
+        return false;
+    }
+
+    public void setClick(Boolean click) {
+        this.click = click;
+    }
+
+    public Boolean getClick() {
+        return click;
+    }
+
     public Image getImage() {
         return image;
     }
@@ -96,6 +119,58 @@ public class MapHome {
 
     public int getY() {
         return y;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public Boolean getCherry() {
+        return cherry;
+    }
+
+    public Boolean getFreeze() {
+        return freeze;
+    }
+
+    public Boolean getSunflower() {
+        return sunflower;
+    }
+
+    public Boolean getPea() {
+        return pea;
+    }
+
+    public Boolean getPotato() {
+        return potato;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setFreeze(Boolean freeze) {
+        this.freeze = freeze;
+    }
+
+    public void setCherry(Boolean cherry) {
+        this.cherry = cherry;
+    }
+
+    public void setPea(Boolean pea) {
+        this.pea = pea;
+    }
+
+    public void setSunflower(Boolean sunflower) {
+        this.sunflower = sunflower;
+    }
+
+    public void setPotato(Boolean potato) {
+        this.potato = potato;
     }
 
     public void setImage(Image image) {
