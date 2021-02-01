7cffa90dd3b6cc5dbc4cd57c8ef89e69b6a26f5e
khosravi-am <58389845+khosravi-am@users.noreply.github.com>
2021-02-01 18:45:00 +0330

    update images

--- src/khosro/model/res/AddressStore.java
+++ src/khosro/model/res/AddressStore.java
@@ -4,8 +4,8 @@ public class AddressStore {
     public static final String MAINPAGE = "./src/khosro/model/res/MainBackground.png";
     public static final String GAMEICONE = "./src/khosro/model/res/icon.png";
     public static final String SETTINGPAGE = "./src/khosro/model/res/MainBackground.png";
-    public static final String LOADPAGE = "./src/khosro/model/res/TextBoxField.png";
-    public static final String SCOREBOARDPAGE = "./src/khosro/model/res/TextBoxField.png";
+    public static final String LOADPAGE = "./src/khosro/model/res/MainBackground.png";
+    public static final String SCOREBOARDPAGE = "./src/khosro/model/res/MainBackground.png";
     public static final String NEWGAMESELECTED = "./src/khosro/model/res/newGame.png";
     public static final String LOADGAMESELECTED = "./src/khosro/model/res/loadGame.png";
     public static final String SCOREBOARDSELECTED = "./src/khosro/model/res/scoreBoard.png";
