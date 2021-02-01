0c880cf9f909f2004ba0d29867426cc00371c955
khosravi-am <58389845+khosravi-am@users.noreply.github.com>
2021-02-01 18:38:41 +0330

    update map

--- src/khosro/model/map/MapHome.java
+++ src/khosro/model/map/MapHome.java
@@ -1,20 +1,24 @@
 package khosro.model.map;
 
+import khosro.model.component.Bullet.Bullet;
 import khosro.model.component.plant.Plants;
+import khosro.model.component.zombie.Zombie;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class MapHome {
     private Image image;
-    private int count, x, y, x2, y2, width, height;
-    private Boolean click = false, sunflower = false, pea = false, freeze = false, potato = false, cherry = false, por = false;
+    private int count, x,y,x2,y2, width, height,row;
+    private Boolean click=false,sunflower=false,pea=false,freeze=false,potato=false,cherry=false,por=false,haveZombie=false,haveBullet=false,haveSnow=false;
     private Plants plant;
-
-    public MapHome(int count, int y, int height) {
+    private Zombie zombie;
+    private Bullet bullet,snowBullet;
+    public MapHome(int count,int y,int height) {
         this.count = count;
-        this.y = y;
-        this.height = height;
+        this.y=y;
+        this.height=height;
         setXY(count);
         setImage(count);
       /*  setFocusable(false);
@@ -23,58 +27,140 @@ public class MapHome {
         if (y>115)
             setBounds(x,y-30,width-10,height);
       */  //setFocusPainted(false);
-        x2 = x + width;
-        y2 = y + height;
+        x2=x+width;
+        y2=y+height;
+    }
+
+    public void setZombie(Zombie zombie) {
+        this.zombie = zombie;
+        setHaveZombie();
+        if (haveZombie) {
+            this.zombie.setX(x2);
+            this.zombie.setY(y-10);
+        }
+
+    }
+
+    public Bullet getBullet() {
+        return bullet;
+    }
+
+    public Zombie getZombie() {
+        return zombie;
+    }
+
+    public void setHaveBullet(){
+        if (bullet==null)
+            haveBullet=false;
+        else haveBullet=true;
+    }
+
+    public void setHaveSnow() {
+        if (snowBullet==null)
+            haveSnow=false;
+        else haveSnow=true;
+    }
+
+    public Boolean getHaveSnow() {
+        return haveSnow;
+    }
+
+    public Bullet getSnowBullet() {
+        return snowBullet;
+    }
+
+    public void setSnowBullet(Bullet snowBullet) {
+        this.snowBullet = snowBullet;
+        setHaveSnow();
+        if (haveSnow) {
+            snowBullet.setX(getX() + 85);
+            if (getY() > 110)
+                snowBullet.setY(getY() + 20);
+            else snowBullet.setY(getY() + 40);
+        }
+    }
+
+    public void setBullet(Bullet bullet) {
+        this.bullet = bullet;
+        setHaveBullet();
+        if (haveBullet) {
+            bullet.setX(getX()+85);
+            if(getY()>110)
+                bullet.setY(getY()+20);
+            else bullet.setY(getY()+40);
+        }
+    }
+
+    public Boolean getHaveBullet() {
+        return haveBullet;
+    }
+
+    public void setHaveZombie() {
+        if (zombie!=null)
+            haveZombie=true;
+        else haveZombie=false;
+    }
+
+    public Boolean getHaveZombie() {
+        return haveZombie;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getRow() {
+        return row;
     }
 
     private void setXY(int count) {
         if (count == 1) {
             x = 35;
-            width = 125;
+            width=125;
         }
 
         if (count == 2) {
             x = 155;
-            width = 110;
-            if (y > 500) {
+            width=110;
+            if (y>500) {
                 x = 150;
-                width = 117;
+                width=117;
             }
         }
         if (count == 3) {
             x = 260;
-            width = 125;
-            if (y > 500) {
-                x = 255;
-                width = 135;
+            width=125;
+            if (y>500){
+                x=255;
+                width=135;
             }
         }
         if (count == 4) {
             x = 380;
-            width = 107;
+            width=107;
         }
         if (count == 5) {
             x = 475;
-            width = 133;
+            width=133;
         }
         if (count == 6) {
             x = 595;
-            width = 123;
+            width=123;
         }
         if (count == 7) {
             x = 705;
-            width = 120;
+            width=120;
         }
         if (count == 8) {
             x = 810;
-            width = 123;
+            width=123;
         }
 
         if (count == 9) {
             x = 915;
-            width = 125;
-            if (y > 100)
-                width = 137;
+            width=125;
+            if (y>100)
+                width=137;
         }
 
     }
@@ -84,7 +170,7 @@ public class MapHome {
 
     }
 
-    public Boolean isPor() {
+    public Boolean isPor(){
         return por;
     }
 
@@ -182,9 +268,13 @@ public class MapHome {
 
     public void setPlant(Plants plan) {
 
-        if (plan != null && plan.getClass().getSimpleName().equals("Cherry"))
+        if (plan!=null && plan.getClass().getSimpleName().equals("Cherry"))
             plan.setGif(new ImageIcon("./src/khosro/model/res/cherry.gif"));
-        plant = plan;
+        if (plan!=null) {
+            plan.setX(getX());
+            plan.setY(getY());
+        }
+        plant=plan;
         setPor();
     }
 
--- src/khosro/model/map/MapRow.java
+++ src/khosro/model/map/MapRow.java
@@ -8,8 +8,9 @@ import java.util.ArrayList;
 
 public class MapRow {
     private ArrayList<MapHome> mapHomes;
-    private Image image,image2;
+    private Image image, image2;
     private int count, x, width, y, height;
+    private Boolean haveZombie = false,haveCar=true;
 
     public MapRow(int count) {
         this.count = count;
@@ -21,20 +22,29 @@ public class MapRow {
     private void setImage() {
         try {
             image = ImageIO.read(new File("./src/khosro/model/res/car.png"));
-            if (count==0)
-                image2=ImageIO.read(new File("./src/khosro/model/res/row-1.png"));
-            if (count==1)
-                image2=ImageIO.read(new File("./src/khosro/model/res/row-2.png"));
-            if (count==2)
-                image2=ImageIO.read(new File("./src/khosro/model/res/row-3.png"));
-            if (count==3)
-                image2=ImageIO.read(new File("./src/khosro/model/res/row-4.png"));
-            if (count==4)
-                image2=ImageIO.read(new File("./src/khosro/model/res/row-5.png"));
+            if (count == 0)
+                image2 = ImageIO.read(new File("./src/khosro/model/res/row-1.png"));
+            if (count == 1)
+                image2 = ImageIO.read(new File("./src/khosro/model/res/row-2.png"));
+            if (count == 2)
+                image2 = ImageIO.read(new File("./src/khosro/model/res/row-3.png"));
+            if (count == 3)
+                image2 = ImageIO.read(new File("./src/khosro/model/res/row-4.png"));
+            if (count == 4)
+                image2 = ImageIO.read(new File("./src/khosro/model/res/row-5.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public void setHaveCar(Boolean haveCar) {
+        this.haveCar = haveCar;
+    }
+
+
+    public Boolean getHaveCar() {
+        return haveCar;
     }
 
     public Image getImage2() {
@@ -42,6 +52,7 @@ public class MapRow {
     }
 
     private void setXY(int count) {
+        x=-30;
         if (count == 0) {
             y = 95;
             height = 147;
@@ -63,6 +74,60 @@ public class MapRow {
             height = 145;
         }
 
+    }
+
+    public int size() {
+        int j = 0;
+        for (int i = 0; i < 9; i++)
+            if (mapHomes.get(i).isPor())
+                j++;
+        return j;
+    }
+
+    public int sunFlowerNumber() {
+        int j = 0;
+        for (int i = 0; i < 9; i++)
+            if (mapHomes.get(i).isPor() && mapHomes.get(i).getPlant().getClass().getSimpleName().equals("SunFlower"))
+                j++;
+        return j;
+    }
+
+    public int freezeNumber() {
+        int j = 0;
+        for (int i = 0; i < 9; i++)
+            if (mapHomes.get(i).isPor() && mapHomes.get(i).getPlant().getClass().getSimpleName().equals("SnowPea"))
+                j++;
+        return j;
+    }
+
+    public int peaNumber() {
+        int j = 0;
+        for (int i = 0; i < 9; i++)
+            if (mapHomes.get(i).isPor() && mapHomes.get(i).getPlant().getClass().getSimpleName().equals("PeaShooter"))
+                j++;
+        return j;
+    }
+
+    public int potatoNumber() {
+        int j = 0;
+        for (int i = 0; i < 9; i++)
+            if (mapHomes.get(i).isPor() && mapHomes.get(i).getPlant().getClass().getSimpleName().equals("Potato"))
+                j++;
+        return j;
+    }
+
+    public void setHaveZombie() {
+
+        for (int i = 0; i < 9; i++)
+            if (mapHomes.get(i).getHaveZombie()) {
+                haveZombie = true;
+                return;
+            }
+        haveZombie = false;
+    }
+
+    public Boolean getHaveZombie() {
+        return haveZombie;
     }
 
     public void setImage(Image image) {
@@ -101,11 +166,16 @@ public class MapRow {
         mapHomes = new ArrayList<>();
         for (int i = 0; i < 9; i++) {
             mapHomes.add(new MapHome(i + 1, y, height));
+            mapHomes.get(i).setRow(count);
             try {
                 mapHomes.get(i).setImage(ImageIO.read(new File("./src/khosro/model/res/" + String.valueOf(count) + "," + String.valueOf(i) + ".png")));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    public void setX(int x) {
+        this.x = x;
     }
 }
